028057d7bdbe40444f45b460dba40c42ac4af790
Wojtek <admin@admins-iMac-2.local>
2018-03-05 07:50:10 +0100

    change if statement in todolist add message that todo was added

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild, ElementRef  } from "@angular/core";
 import {trigger, state, style, transition, animate, keyframes, query, stagger } from '@angular/animations';
 import {Meta, Title } from "@angular/platform-browser";
 import { TodoService } from "../../services/todo.service";
+import { Router } from "@angular/router";
 @Component({
     selector: "app-home",
     templateUrl: "./home.component.html",
@@ -9,13 +10,27 @@ import { TodoService } from "../../services/todo.service";
 })
 export class HomeComponent implements OnInit {
     private todoText: string;
-    constructor(private todoService: TodoService) {
+    show: boolean = false;
+    constructor(private todoService: TodoService, private router: Router) {
         this.todoText = "";
     }
     ngOnInit(){
     }
     private addTodo(): void {
-        this.todoService.addTodo(this.todoText);
-        this.todoText = '';
+        if(this.todoText == "clearAll()") { //hidden function
+            localStorage.clear();
+        }else{
+            this.todoService.addTodo(this.todoText);
+            this.todoText = '';
+            this.show = true;
+            setTimeout(() => {
+                this.show = false;
+            }, 3000);
+        }
+    }
+    keyDownFunction(event) {
+        if(event.keyCode == 13) {
+            this.addTodo();
+        }
     }
 }
